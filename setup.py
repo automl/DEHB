b84bae2d738e3ea59993390c45206db4e908e19c
neeratyoy <neeratyoy@gmail.com>
2022-06-13 14:32:44 +0200

    Adding https://github.com/automl/automl_template

--- setup.py
+++ setup.py
@@ -2,15 +2,21 @@ import os
 
 import setuptools
 
-from dehb import (
-    author,
-    author_email,
-    description,
-    package_name,
-    project_urls,
-    url,
-    version,
-)
+import datetime
+
+
+name = "DEHB"
+package_name = "dehb"
+author = "Neeratyoy, Noor, Frank"
+author_email = "mallik@cs.uni-freiburg.de"
+description = "Evolutionary Hyperband for Scalable, Robust and Efficient Hyperparameter Optimization"
+url = "https://github.com/automl/DEHB"
+project_urls = {
+    "Documentation": "https://automl.org.github.io/DEHB/main",
+    "Source Code": "https://github.com/automl.org/dehb",
+}
+copyright = f"Copyright {datetime.date.today().strftime('%Y')}, Neeratyoy, Noor, Frank"
+version = "0.0.1"
 
 HERE = os.path.dirname(os.path.realpath(__file__))
 
@@ -57,7 +63,7 @@ setuptools.setup(
     author=author,
     author_email=author_email,
     description=description,
-    long_description=read_file(os.path.join(HERE, "../README.md")),
+    long_description=read_file(os.path.join(HERE, "README.md")),
     long_description_content_type="text/markdown",
     license="Apache-2.0",
     url=url,
@@ -65,7 +71,7 @@ setuptools.setup(
     version=version,
     packages=setuptools.find_packages(exclude=["tests"]),
     python_requires=">=3.8",
-    install_requires=read_file(os.path.join(HERE, "../requirements.txt")).split("\n"),
+    install_requires=read_file(os.path.join(HERE, "requirements.txt")).split("\n"),
     extras_require=extras_require,
     test_suite="pytest",
     platforms=["Linux"],
