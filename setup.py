50563d43f491aefa09c4abdecdc67752ee6c90e9
neeratyoy <neeratyoy@gmail.com>
2022-06-13 19:45:20 +0200

    Updating README for pip packaging

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ project_urls = {
     "Source Code": "https://github.com/automl.org/dehb",
 }
 copyright = f"Copyright {datetime.date.today().strftime('%Y')}, Neeratyoy, Noor, Frank"
-version = "0.0.1"
+version = "0.0.2"
 
 HERE = os.path.dirname(os.path.realpath(__file__))
 
