81426af99ad95e2285dc9d4ce748b36e04142540
neeratyoy <neeratyoy@gmail.com>
2022-08-17 14:57:51 +0200

    New version 0.0.3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ project_urls = {
     "Source Code": "https://github.com/automl.org/dehb",
 }
 copyright = f"Copyright {datetime.date.today().strftime('%Y')}, Neeratyoy, Noor, Frank"
-version = "0.0.2"
+version = "0.0.3"
 
 HERE = os.path.dirname(os.path.realpath(__file__))
 
