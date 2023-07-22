1b346949f9e240b282ad9f0163c781bcd027b2d6
Janis Fix <janis_fix@web.de>
2023-07-22 14:44:38 +0200

    Fix precommit and coverage pipeline according to folder structure change

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,28 +1,27 @@
 import importlib
 
+import pytest
+
+
 class TestImports():
-    """This class bundles all import tests for the DEHB project.
-    """
+    """This class bundles all import tests for the DEHB project."""
     def test_dehb_import(self):
-        """Test if DEHB can be imported properly.
-        """
+        """Test if DEHB can be imported properly."""
         try:
             importlib.import_module("src.dehb.optimizers.dehb")
         except ImportError as e:
-            assert False, f"Failed to import dehb: {e}"
+            pytest.fail(f"Failed to import dehb: {e}")
 
     def test_de_import(self):
-        """Test if DE can be imported properly.
-        """
+        """Test if DE can be imported properly."""
         try:
             importlib.import_module("src.dehb.optimizers.de")
         except ImportError as e:
-            assert False, f"Failed to import de: {e}"
+            pytest.fail(f"Failed to import de: {e}")
 
     def test_utils_import(self):
-        """Test if BracketManager can be imported properly.
-        """
+        """Test if BracketManager can be imported properly."""
         try:
             importlib.import_module("src.dehb.utils")
         except ImportError as e:
-            assert False, f"Failed to import dehb: {e}"
+            pytest.fail(f"Failed to import utils: {e}")
